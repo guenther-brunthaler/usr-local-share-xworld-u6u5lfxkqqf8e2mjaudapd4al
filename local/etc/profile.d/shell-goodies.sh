bd0466a6ce2a1003ea0d7420fa9e69334f45586b
Guenther Brunthaler <root@xqrescue.xworld.mine.nu>
2008-07-26 18:30:33 +0200

    Added automatic sourcing of shell goodies framework via Gentoo hook

---
+++ local/etc/profile.d/shell-goodies.sh
@@ -0,0 +1,5 @@
+#! /bin/false
+# Add support for extensible bash- and shell-plugins.
+# Despite the name, should work for most Bourne-derived
+# shells, not just for bash.
+. /usr/local/etc/site/bash-goodies
