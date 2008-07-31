9e2cd58e818c078078f3b42093646192ee0f4dc1
Guenther Brunthaler <root@xqrescue.xworld.mine.nu>
2008-07-31 12:02:31 +0200

    Removed automatic sourcing of bash-goodies from /etc/profile
    It turns out that it must be sourced from ~/.bashrc instead,
    or it will not work correctly for non-login interactive shells.

--- local/etc/profile.d/shell-goodies.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/false
-# Add support for extensible bash- and shell-plugins.
-# Despite the name, should work for most Bourne-derived
-# shells, not just for bash.
-. /usr/local/etc/site/bash-goodies
