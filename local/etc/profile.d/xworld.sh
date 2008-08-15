c8bc57477c3ffcabde9ae8537d5d5907a008403c
Guenther Brunthaler <root@xquad.xworld.mine.nu>
2008-08-15 21:19:45 +0200

    Added another set of optional directories to PATH
    The following subdirectories of /usr/local/(s)bin will now also
    be prepended to $PATH if they exist:
    
    machine_local (not published, local machine only)
    site_internal (non-published, local administrative realm only)
    xworld_internal (non-published, xworld.mine.nu realm only)
    xworld (published by xworld.mine.nu)

--- local/etc/profile.d/xworld.sh
+++ local/etc/profile.d/xworld.sh
@@ -2,8 +2,30 @@
 # Add "xworld"-site binaries to $PATH.
 
 
-PATH=/usr/local/bin/xworld${PATH:+:}$PATH
-if test x`id -u` = x0; then
-	PATH=/usr/local/sbin/xworld${PATH:+:}$PATH
-fi
+PATH=`(
+	OPATH=$PATH
+	PATH=/sbin/:/bin:/usr/sbin:/usr/bin
+	set --
+	# Add significant directory name components in the order to be added.
+	test x\`id -u\` = x0 && set -- "$@" sbin
+	set -- "$@" bin
+	NPATH=
+	for BIN in "$@"; do
+		# Subdirectories to be added in the order to be added.
+		# But in any case, only directories which actually exist
+		# will be added!
+		for SUB in \
+			machine_local \
+			site_internal \
+			xworld_internal \
+			xworld
+		do
+			NEW=/usr/local/$BIN/$SUB
+			test -d "$NEW" || continue
+			NPATH=$NPATH${NPATH:+:}$NEW
+		done
+	done
+	test -n "$OPATH" && test -n "$NPATH" && NPATH=$NPATH:
+	printf "%s\n" "$NPATH$OPATH"
+)`
 export PATH
