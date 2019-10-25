6c6a82fc73d08de8c86db001c95d47413245e894
Guenther Brunthaler <root@xduo.emgenxx69lwyn5ctlr4nl64ul.local>
2019-10-25 03:01:10 +0200

    Remove global snippet hooks
    Move them to the same version control branch where all the other
    snippets have been moved some commits ago.
    
    Users should not be patronized and decide themselves if they want to
    use the snippet framework or not.

--- local/etc/profile.d/path_adm_bin.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/false
-# Inserts bin directory for additional system-wide binaries into $PATH.
-
-set -- \
-	--append /home/group/admdevel/bin \
-	--append /home/project/admdevel/bin \
-	--stop "$@"
-. /usr/local/libexec/xworld/sh/path_hyec3v5m8kd1vjs8k7d1wce62.sh
